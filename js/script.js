61dd34e39923c7dd5001c576a39d9254e9779ebd
Marcos Salazar <Marcos1010521@gmail.com>
2020-12-14 17:19:50 -0400

    done carrosel and footer

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+goTo = a=>{
+    if(a == "watch"){
+        window.open("https://bit.ly/3gNjc19","blank");
+    }
+    if(a == "moreInfo"){
+        window.open("https://bit.ly/3qYNWR8","blank")
+    }
+}
+
+socialMedia = a=>{
+    if(a == "linkedIn"){
+        window.open("https://www.linkedin.com/in/marcosfernandessalazar","blank");
+    }
+    if(a == "gitHub"){
+        window.open("https://github.com/Marcosfsalazar","blank")
+    }
+}
